591d78dc1595b888a1bc93a0b40a27855966c5fa
AndreyBibikov <andrey.bibikoff@yandex.ru>
2022-03-01 23:35:46 +0400

    Lesson_2 v1

--- todoservice/settings.py
+++ todoservice/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 
     'users',
     'rest_framework',
@@ -58,7 +59,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
 ]
+
 
 ROOT_URLCONF = 'todoservice.urls'
 
@@ -132,3 +137,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+   'http://localhost:3000',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
